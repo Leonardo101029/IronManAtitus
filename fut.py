b4f9f5cac8ddc1ef444a801218204429a3294ac0
Leonardo Alves <leonardo1029leonardo1029@gmail.com>
2022-11-06 00:43:19 -0300

    v2 com placar

--- fut.py
+++ fut.py
@@ -21,13 +21,23 @@ fundo = pygame.image.load("assets/campo.jpg")
 bola = pygame.image.load("assets/bola.png")
 tamanhoxbola = 50
 tamanhoybola = 50
-posicaobolax = 225
-posicaobolay = 305
+posicaobolax = 202
+posicaobolay = 283
 movimentobolax = 0
 movimentobolay = 0
 velocidade = 5
 #looping para ficar rodando o jogo
 jogando = True
+
+placarPlayer1 = 0 
+placarPlayer2 = 0
+
+def escreverTexto(texto):
+    fonte = pygame.font.Font("freesansbold.ttf", 15)
+    textoDisplay = fonte.render(texto, True, branco)
+    display.blit(textoDisplay, (5,5))
+
+
 
 while jogando:
     for evento in pygame.event.get():
@@ -49,7 +59,7 @@ while jogando:
     if posicaobolax + movimentobolax + tamanhoxbola < largura and posicaobolax + movimentobolax > 0:
         posicaobolax = posicaobolax + movimentobolax
    
-    if posicaobolay + movimentobolay + tamanhoybola < altura and posicaobolay + movimentobolay >0:
+    if posicaobolay + movimentobolay + tamanhoybola< altura and posicaobolay + movimentobolay >0:
         posicaobolay = posicaobolay + movimentobolay
 
     display.fill(branco)
@@ -57,7 +67,18 @@ while jogando:
     posicao = (posicaobolax, posicaobolay)
     display.blit(bola, (posicaobolax, posicaobolay))
     #pygame.draw.circle(display, preto, posicao, 10)
-    
+
+    if posicaobolay <8 and posicaobolax > 173 and posicaobolax <279:
+        placarPlayer1 += 1
+        posicaobolax = 202
+        posicaobolay = 283
+         
+    if posicaobolay >552 and posicaobolax > 173 and posicaobolax <279:
+        placarPlayer2 += 1
+        posicaobolax = 202
+        posicaobolay = 283
+
+    escreverTexto('Placar'+ str(placarPlayer1+' x '+str(placarPlayer2)))
     pygame.display.update()
     fps.tick(60)
 
